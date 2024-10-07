34106bbbe88bef2e04b3e871130090441aae9e78
om3ga <om3ga6400gd@gmail.com>
2024-10-07 12:02:17 -0400

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,9 @@
+character1 = input("Enter a character: ")
+character2 = input("Enter another character: ")
+place = input("Enter a place: ")
+action1 = input("Enter a past tense action: ")
+action2 = input("Enter a tiring future action: ")
+action3 = input("Enter the same action but currently doing it: ")
+
+print("\nHere's your Madlib:\n")
+print(f"{character1} and {character2} went to {place} and {action1}. Afterwards {character2} asked {character1} if they wanted to {action2} tomorrow and {character1} agreed. When the next day arived, {character1} and {character2} started {action3} until they got very tired and whent home. When {character1} and {character2} got home they immediately fell asleep")
